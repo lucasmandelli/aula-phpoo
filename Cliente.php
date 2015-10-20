e0f7b500df7c2694da187bfacb6d53d4ba32160a
Lucas Mandelli <lucasmandelli@gmail.com>
2015-10-19 22:38:33 -0200

    Atualizacao da Classe Cliente e Criado arquivo index.php

--- Cliente.php
+++ Cliente.php
@@ -2,14 +2,21 @@
 
 class Cliente
 {
+    public $id;
     public $nome;
     public $email;
-    public $cpf;
+    public $telefone;
     public $endereco;
 
 
-    public function __construct()
+    public function __construct($id, $nome, $email, $telefone, $endereco)
     {
-        
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->email = $email;
+        $this->telefone = $telefone;
+        $this->endereco = $endereco;
     }
+
+
 }
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once("Cliente.php");
+
+
+$objCliente1 = new Cliente(1, 'Rafaela Cavalcanti Ferreira', 'rafa_c_ferreira@gmail.com', '(65) 6977-5350', 'Rua Bianco Filho, 1014');
+$objCliente2 = new Cliente(2, 'Fernando Daniel Ribeiro', 'fdribeiro@gmail.com', '(38) 4301-5610', 'Travessa Martins Teixeira, 865');
+$objCliente3 = new Cliente(3, 'Nicole Rocha Sousa', 'nrs@gmail.com', '(62) 8361-5004', 'Rua das Oliveiras, 1094');
+$objCliente4 = new Cliente(4, 'Júlia Costa Pinto', 'juliacosta@gmail.com', '(81) 9944-8781', 'Avenida Fator, 456');
+$objCliente5 = new Cliente(5, 'Alex Rocha Barros', 'alexrb@gmail.com', '(91) 7167-6031', 'Alameda N, 1799');
+$objCliente6 = new Cliente(6, 'José Almeida Santos', 'josesantos@gmail.com', '(11) 8927-2070', 'Rua Paraibuna, 425');
+$objCliente7 = new Cliente(7, 'Maria Carvalho Cunha', 'mariacunha@gmail.com', '(21) 3808-4841', 'Rua Reinaldo Lópes, 299');
+$objCliente8 = new Cliente(8, 'Fernanda Ribeiro Pinto', 'feripi@gmail.com', '(46) 4233-3405', 'Rua das Hortênsias, 773');
+$objCliente9 = new Cliente(9, 'Nicolas Goncalves Cavalcanti', 'gonca@gmail.com', '(43) 3098-5985', 'Rua Anu, 1258');
+$objCliente10 = new Cliente(10, 'Thiago Pereira Cardoso', 'cardoso@gmail.com', '(11) 5948-6115', 'Praça Ministro Salgado Filho, 1997');
+
+
+
