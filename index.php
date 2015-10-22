2970b135db73b363dd520335a1b98be3553b73cf
Lucas Mandelli <lucasmandelli@gmail.com>
2015-10-21 23:36:22 -0200

    Ordenação lista

--- index.php
+++ index.php
@@ -12,13 +12,18 @@ include('dados.php');
 
         <!-- Bootstrap -->
         <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="vendor/bootstrap-table-master/dist/bootstrap-table.css" rel="stylesheet">
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <script type="text/javascript" src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script type="text/javascript" src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script type="text/javascript" src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="vendor/bootstrap-table-master/dist/bootstrap-table.min.js"></script>
     </head>
     <body>
         <div class="row">
@@ -31,10 +36,10 @@ include('dados.php');
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-offset-1 col-md-10 col-lg-offset-2 col-lg-8">
                 <div class="table-responsive">
-                    <table class="table table-hover">
+                    <table data-toggle="table" class="table table-hover">
                         <thead>
                         <tr>
-                            <th>#</th>
+                            <th data-sortable="true">#</th>
                             <th>Nome</th>
                             <th>E-mail</th>
                             <th>Telefone</th>
